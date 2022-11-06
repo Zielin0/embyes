47fcffc0e0d3db386992bddbe7f2636389f07439
Zielin0 <lamaxdmilo@gmail.com>
2022-11-06 23:18:56 +0100

    docs(server): Todo

--- server/main.py
+++ server/main.py
@@ -35,6 +35,7 @@ class OGMeta:
 
     # @TODO: Add images and possibly links
     # @TODO: would need database tho cause links will be too long
+    # @TODO: If adding a database consider adding custom links and their expiry
     def format(self, color: str, title: str, description: str) -> str:
         if self.check(color, title, description):
             return self.template % (color, title, description)
