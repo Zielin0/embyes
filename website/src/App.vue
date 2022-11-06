d3086c3443b622e0aba09116bb0e6b1982e87513
Zielin0 <lamaxdmilo@gmail.com>
2022-11-06 19:26:23 +0100

    feat(App): Added copy to clipboard button

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,33 +1,50 @@
 <script setup lang="ts">
-// @TODO: move it to ./modules/OGModule.ts
-// @TODO: Also rename this to OGModule
-class Embed {
-  constructor(
-    public color: string,
-    public title: string,
-    public description: string
-  ) {
-    this.color = color;
-    this.title = title;
-    this.description = description;
-  }
-}
+import OGModule from './modules/OGModule';
 
-const embed = new Embed('', '', '');
+const ogModule: OGModule = new OGModule('', '', '');
 
 const match = /^#(?:[0-9a-fA-F]{3}){1,2}$/;
 
-// @TODO: Add a link component
+// @TODO: Move URL to .env
+const url: string = 'http://localhost:6969';
+
+function copy() {
+  const newURL = `${url}/embed?color=${ogModule.color.replace('#', '')}&title=${
+    ogModule.title
+  }&description=${ogModule.description}`;
+
+  navigator.clipboard.writeText(newURL);
+}
+
 function create() {
-  // eslint-disable-next-line no-console
-  console.log(embed);
+  ogModule.title = ogModule.title.replaceAll(' ', '%20');
+  ogModule.description = ogModule.description.replaceAll(' ', '%20');
+
+  const parent = document.getElementById('form');
+
+  if (!document.getElementById('copy')) {
+    const copyButton = document.createElement('button');
+    copyButton.type = 'button';
+    copyButton.className = 'secondary';
+    copyButton.id = 'copy';
+    copyButton.innerText = 'Copy URL to Clipboard';
+    copyButton.addEventListener('click', () => {
+      copy();
+    });
+
+    parent?.appendChild(copyButton);
+  }
 }
 
 function validate() {
-  const matchColor: boolean = embed.color !== '' && match.test(embed.color);
-  const matchTitle: boolean = embed.title !== '' && embed.title.length <= 64;
+  const matchColor: boolean =
+    ogModule.color !== '' && match.test(ogModule.color);
+
+  const matchTitle: boolean =
+    ogModule.title !== '' && ogModule.title.length <= 64;
+
   const matchDesc: boolean =
-    embed.description !== '' && embed.description.length <= 1024;
+    ogModule.description !== '' && ogModule.description.length <= 1024;
 
   document
     .getElementById('color')
@@ -52,25 +69,25 @@ function validate() {
     <h1 class="center">Create Your Custom Embed</h1>
     <form id="form" @submit.prevent="validate()">
       <div class="grid">
-        <label for="color"
-          >Color
-          <input v-model="embed.color" id="color" type="color" />
+        <label for="color">
+          Color
+          <input v-model="ogModule.color" id="color" type="color" />
         </label>
 
-        <label for="title"
-          >Title
+        <label for="title">
+          Title
           <input
-            v-model="embed.title"
+            v-model="ogModule.title"
             type="text"
             id="title"
             maxlength="64"
-            placeholder="Cool Embed!"
+            placeholder="Cool Title!"
           />
         </label>
       </div>
       <label for="desc">Description</label>
       <input
-        v-model="embed.description"
+        v-model="ogModule.description"
         type="text"
         id="desc"
         maxlength="1024"
