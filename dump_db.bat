a955df9750661720ea6e543dbeee3dfcc53e3e87
Zielin0 <me@zielinus.xyz>
2022-11-19 19:23:57 +0100

    chore(dump_db): Check if 'data/' folder exists

--- dump_db.bat
+++ dump_db.bat
@@ -2,6 +2,10 @@
 
 echo Dumping latest Database...
 
+if not exist "data" (
+  mkdir data
+)
+
 docker exec -t postgres-container pg_dump -U embyes --table="embeds" --data-only --column-inserts embeds > data/dump_latest.sql
 
 echo Database Dumped
