4e835d4a7e2c0b015cad22559509c783aebc19c6
Zielin0 <me@zielinus.xyz>
2022-11-19 16:33:56 +0100

    fix(db): Made url unique

--- db/sql/init.sql
+++ db/sql/init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE embeds (
-  url varchar(48) not null,
+  url varchar(48) not null unique,
   color varchar(6) not null,
   title varchar(64) not null,
   description varchar(256) not null,
