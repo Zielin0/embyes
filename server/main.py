41cfa58d1f443ea92f964fbdf95a4c694397816e
Zielin0 <me@zielinus.xyz>
2022-11-19 22:42:22 +0100

    feat(server): Return OGModule according to its URL on '/<url>' route

--- server/main.py
+++ server/main.py
@@ -2,7 +2,9 @@ from db.Database import Database
 from flask import Flask, request
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from modules.OGModule import OGModule
 from psycopg2.errors import UniqueViolation
+from template.Template import TemplateFile
 
 app = Flask(__name__)
 limiter = Limiter(app, key_func=get_remote_address)
@@ -13,6 +15,35 @@ db: Database = None
 @app.route("/")
 def index() -> str:
     return "Hello, World!", 200
+
+
+@app.route("/<url>")
+def lol(url: str) -> str:
+    template: OGModule = None
+
+    query = "SELECT * FROM embeds WHERE url = %s"
+    db.cursor.execute(query, (url, ))
+
+    row = db.cursor.fetchall()
+    color = row[0][1]
+    title = row[0][2]
+    description = row[0][3]
+
+    image = row[0][4]
+    small = row[0][5]
+
+    if None in (image, small):
+        template = OGModule(TemplateFile.TEMPLATE_BASIC)
+        try:
+            return template.format(f"#{color}", title, description)
+        except ValueError as e:
+            return f"{e}"
+
+    template = OGModule(TemplateFile.TEMPLATE_FULL)
+    try:
+        return template.format_full(f"#{color}", title, description, image, small)
+    except ValueError as e:
+        return f"{e}"
 
 
 @app.route("/new", methods=["POST", "GET"])
