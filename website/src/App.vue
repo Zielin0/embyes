0e673f2c175f57a9236c34c57b7869d1cb3b8765
Zielin0 <me@zielinus.xyz>
2022-11-23 15:53:17 +0100

    feat(website): creating a embed in db with data from form

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,120 +1,90 @@
 <script setup lang="ts">
-import OGModule from './modules/OGModule';
+const baseUrl: string = import.meta.env.VITE_URL;
 
-const ogModule: OGModule = new OGModule('', '', '');
+async function create(submitEvent: any) {
+  const { elements } = submitEvent.target;
+  const { color, title, url, desc, image, small } = elements;
 
-const hexPattern = /^#(?:[0-9a-fA-F]{3}){1,2}$/;
-
-// @TODO: Move URL to .env
-const url: string = 'http://localhost:6969';
-
-let reqURL: string;
-
-function copy() {
-  const copyElement = document.getElementById('copy');
-  if (copyElement) {
-    copyElement.setAttribute('aria-busy', 'true');
-    copyElement.innerText = 'Copying...';
-
-    setTimeout(() => {
-      navigator.clipboard.writeText(reqURL);
-
-      copyElement.setAttribute('aria-busy', 'false');
-      copyElement.innerText = 'Copied!';
-    }, 300);
-  }
-}
-
-function create() {
-  ogModule.title = ogModule.title.replaceAll(' ', '%20');
-  ogModule.description = ogModule.description.replaceAll(' ', '%20');
-
-  reqURL = `${url}/og?color=${ogModule.color.replace('#', '')}&title=${
-    ogModule.title
-  }&description=${ogModule.description}`;
-
-  const parent = document.getElementsByClassName('grid')[1];
-
-  if (!document.getElementById('copy')) {
-    const copyButton = document.createElement('button');
-    copyButton.type = 'button';
-    copyButton.className = 'secondary';
-    copyButton.id = 'copy';
-    copyButton.innerText = 'Copy URL to Clipboard';
-    copyButton.addEventListener('click', () => {
-      copy();
-    });
-
-    const linkField = document.createElement('textarea');
-    linkField.setAttribute('disabled', '');
-    linkField.placeholder = reqURL;
-
-    parent?.appendChild(copyButton);
-    parent?.appendChild(linkField);
-  }
-}
-
-function validate() {
-  const matchColor: boolean =
-    ogModule.color !== '' && hexPattern.test(ogModule.color);
-
-  const matchTitle: boolean =
-    ogModule.title !== '' && ogModule.title.length <= 64;
-
-  const matchDesc: boolean =
-    ogModule.description !== '' && ogModule.description.length <= 256;
-
-  document
-    .getElementById('color')
-    ?.setAttribute('aria-invalid', `${!matchColor}`);
-
-  document
-    .getElementById('title')
-    ?.setAttribute('aria-invalid', `${!matchTitle}`);
-
-  document
-    .getElementById('desc')
-    ?.setAttribute('aria-invalid', `${!matchDesc}`);
-
-  if (matchColor && matchTitle && matchDesc) {
-    create();
-  }
+  // @TODO: Handling response status codes.
+  // @TODO: Add aria-invalid for certain inputs
+  await fetch(
+    `${baseUrl}/new?${new URLSearchParams({
+      url: url.value,
+      color: color.value.replace('#', ''),
+      title: title.value,
+      description: desc.value,
+      image: image.value,
+      small: small.value,
+    })}`,
+    {
+      method: 'POST',
+    }
+  );
 }
 </script>
 
 <template>
   <main class="container panel">
     <h1 class="center">Create Your Custom Embed</h1>
-    <form id="form" @submit.prevent="validate()">
+    <form id="form" @submit.prevent="create">
       <div class="grid">
         <label for="color">
           Color
-          <input v-model="ogModule.color" id="color" type="color" />
+          <input id="color" type="color" />
         </label>
 
         <label for="title">
           Title
           <input
-            v-model="ogModule.title"
             type="text"
             id="title"
             maxlength="64"
             placeholder="Cool Title!"
           />
         </label>
+
+        <label for="url">
+          Custom URL
+          <input
+            type="text"
+            id="url"
+            maxlength="48"
+            placeholder="goofy-embed"
+          />
+        </label>
       </div>
+
       <label for="desc">Description</label>
       <input
-        v-model="ogModule.description"
         type="text"
         id="desc"
         maxlength="256"
         placeholder="Cool Description :D!"
       />
 
-      <button type="submit">Create!</button>
+      <div class="grid">
+        <label for="image">
+          Image URL
+          <input
+            type="text"
+            id="image"
+            maxlength="128"
+            placeholder="example.com/image.png"
+          />
+        </label>
 
-      <div class="grid"></div>
+        <label for="small">
+          Small Text
+          <input
+            type="text"
+            id="small"
+            maxlength="32"
+            placeholder="Hello Mom!"
+          />
+        </label>
+      </div>
+
+      <button type="submit">Create!</button>
     </form>
   </main>
 </template>
